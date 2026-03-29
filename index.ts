e84037ed184e0d75ed10dac3801802b1f67c4cc6
Eduardo Stuart <e@s.tuart.me>
2026-03-29 17:10:37 +0200

    fix: quote fallback font names in JS config

--- index.ts
+++ index.ts
@@ -19,9 +19,9 @@ const plugin: TulipPlugin = {
     let fontConfig = "";
     if (fonts.sans || fonts.serif || fonts.mono) {
       let extend = [];
-      if (fonts.sans) extend.push("sans:['" + fonts.sans + "',system-ui,sans-serif]");
-      if (fonts.serif) extend.push("serif:['" + fonts.serif + "',Georgia,serif]");
-      if (fonts.mono) extend.push("mono:['" + fonts.mono + "',ui-monospace,monospace]");
+      if (fonts.sans) extend.push("sans:['" + fonts.sans + "','system-ui','sans-serif']");
+      if (fonts.serif) extend.push("serif:['" + fonts.serif + "','Georgia','serif']");
+      if (fonts.mono) extend.push("mono:['" + fonts.mono + "','ui-monospace','monospace']");
       fontConfig = ",theme:{fontFamily:{" + extend.join(",") + "}}";
     }
     let config = '<script>tailwind.config={darkMode:"class"' + fontConfig + '}</script>';
