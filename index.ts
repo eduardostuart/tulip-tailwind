15e2f9f4c1b457c271eabe28db583a01e96310df
Eduardo Stuart <e@s.tuart.me>
2026-03-29 16:43:40 +0200

    feat: add darkMode class config via transformHtml

--- index.ts
+++ index.ts
@@ -13,6 +13,11 @@ const plugin: TulipPlugin = {
       },
     ];
   },
+
+  transformHtml(html: string, page: Page): string {
+    let config = '<script>tailwind.config={darkMode:"class"}</script>';
+    return html.replace("</head>", config + "</head>");
+  },
 };
 
 export default plugin;
